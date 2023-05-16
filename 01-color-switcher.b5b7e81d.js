f5cae41fd85e48030fae91d5a554fd714aab0928
ChernovDan42 <120645560+ChernovDan42@users.noreply.github.com>
2023-05-16 20:55:40 +0000

    Deploying to gh-pages from @ ChernovDan42/goit-js-hw-09@091bfaf99aa732d5e2475ebbfb00b7d8ae58eca0 🚀

--- 01-color-switcher.b5b7e81d.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");e.addEventListener("click",(function(){r.removeAttribute("disabled"),e.setAttribute("disabled",""),d=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),r.addEventListener("click",(function(){clearInterval(d),r.setAttribute("disabled",""),e.removeAttribute("disabled"),isIntervalActive=!1})),r.setAttribute("disabled","");let d=null;
-//# sourceMappingURL=01-color-switcher.b5b7e81d.js.map
